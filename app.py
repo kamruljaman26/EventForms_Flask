eb4903b59db48865433f1f78dca1d3aeefb74b1e
Kamrul Jaman <kamruljaman@Kamruls-MacBook-Pro.local>
2020-10-21 16:24:30 +0600

    Setup Database ORM Module, Added Helper Files

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:JAMANkamrul1@@localhost/eventforms'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 
 @app.route("/")
